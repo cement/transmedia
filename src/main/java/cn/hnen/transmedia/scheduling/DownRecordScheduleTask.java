eb84462862ec192c0040b9fb4857ea32f1c3bb17
Administrator <32463636@qq.com>
2018-12-19 15:39:11 +0800

    添加定时删除数据库逻辑,每周六1点执行一次，删除30天以前的记录。

--- src/main/java/cn/hnen/transmedia/scheduling/DownRecordScheduleTask.java
+++ src/main/java/cn/hnen/transmedia/scheduling/DownRecordScheduleTask.java
@@ -20,7 +20,7 @@ import java.util.Date;
  * @desc  定时任务，目前没有使用，将来删除等业务需要
  */
 @Component
-public class ScheduleTask {
+public class DownRecordScheduleTask {
 
     @Autowired
     private RestTemplate restTemplate;
@@ -33,15 +33,17 @@ public class ScheduleTask {
         System.out.println("This is a say method!"+new Date());
     }
 
-    @Scheduled(cron = "0/5 * * * * ?")
+//    @Scheduled(cron = "0/5 * * * * ?")
+    //每周六一点执行一次
+    @Scheduled(cron = "0 0 1 ? * 7")
     public void deleteDownInfo() {
 
-        Date date = new Date();//当前日期
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");//格式化对象
-        Calendar calendar = Calendar.getInstance();//日历对象
-        calendar.setTime(date);//设置当前日期
-        calendar.add(Calendar.MONTH, -1);//月份减一
-        Date preMonthDate = calendar.getTime();
+//        Date date = new Date();//当前日期
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");//格式化对象
+//        Calendar calendar = Calendar.getInstance();//日历对象
+//        calendar.setTime(date);//设置当前日期
+//        calendar.add(Calendar.MONTH, -1);//月份减一
+//        Date preMonthDate = calendar.getTime();
 
         System.out.println("===================================="+JpaBusinessConfig.overDueDays);
       downloadRepository.deleteOutDateRecord(JpaBusinessConfig.overDueDays);
